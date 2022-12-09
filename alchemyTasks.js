10098bddd301b3736f3f0e4633ade5813b42868c
rizankhan <rizankhan837@gmail.com>
2022-12-09 17:49:18 +0500

    Added Task 2

--- alchemyTasks.js
+++ alchemyTasks.js
@@ -13,3 +13,13 @@
     }
     return arr;
 } */
+
+/* Given an array of integers and a number, num, find all the occurrences of the number and remove it from the array. */
+
+/* function removeOccurrences(array, num) {
+    for(let i= array.length - 1; i>=0; i--){
+        if(array[i] == num){
+            array.splice(i, 1);
+        }
+    }
+} */
