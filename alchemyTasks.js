b2912dac52ca5c0ace517988ecadb1f8b3f1ee8f
rizankhan <rizankhan837@gmail.com>
2022-12-09 18:43:38 +0500

    Updated

--- alchemyTasks.js
+++ alchemyTasks.js
@@ -23,3 +23,21 @@
         }
     }
 } */
+
+/* Given an array of pizza orders, return the total number of pizzas ordered.
+
+The orders are an array of objects, each with pizzas key inside: */
+
+/* const orders = [
+    { pizzas: 3 },
+    { pizzas: 5 },
+    { pizzas: 10 }
+]; */
+
+/* function numberOfPizzas(orders) {
+    let total = 0;
+    for(let i = 0; i < orders.length; i++) {
+        total += orders[i].pizzas;
+    }
+    return total;
+} */
