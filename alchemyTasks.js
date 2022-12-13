53265dedb5e660f7019a32a7be935cfb516334d8
rizankhan <rizankhan837@gmail.com>
2022-12-13 09:18:12 +0500

    Added Another Task

--- alchemyTasks.js
+++ alchemyTasks.js
@@ -58,3 +58,7 @@ Odd numbers should be rounded up to the nearest whole number. */
     }
     return num;
 } */
+
+/* Write a function countVowels that takes in a string and counts how many vowels there are in the word. Vowels include: "a", "e", "i", "o", and "u".
+
+Handle lowercase and uppercase vowels. */
