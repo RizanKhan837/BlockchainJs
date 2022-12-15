66cf5176aa5fb7dbbe5b58a406574019dc419f00
rizankhan <rizankhan837@gmail.com>
2022-12-15 08:20:27 +0500

    Updated

--- alchemyTasks.js
+++ alchemyTasks.js
@@ -63,7 +63,7 @@ Odd numbers should be rounded up to the nearest whole number. */
 
 Handle lowercase and uppercase vowels. */
 
-/* function countVowels(str) {
+function countVowels(str) {
     let count = 0;
     for(let i =0; i< str.length; i++){
         if(str[i].toLowerCase() == "a" || str[i].toLowerCase() == "e" ||str[i].toLowerCase() == "i" ||str[i].toLowerCase() == "o" ||str[i].toLowerCase() == "u"){
@@ -71,4 +71,4 @@ Handle lowercase and uppercase vowels. */
         }
     }
     return count;
-} */
+}
