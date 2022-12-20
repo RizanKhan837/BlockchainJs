f8ef2b9ec65bc06f7e829a7383db1a70a2bc80fa
rizankhan <rizankhan837@gmail.com>
2022-12-20 20:48:34 +0500

    add carCrossing

--- alchemyTasks.js
+++ alchemyTasks.js
@@ -144,3 +144,17 @@ The problem is, sometimes friend is undefined. When this is the case, let's retu
         return undefined;
     }
 } */
+
+/* There's a river that runs right between two towns. In order for cars to cross the river, we built a bridge!
+
+Trouble is, there's only room for 1 car to cross the bridge at a time. This means that if both cars were to be crossing at the same time, neither would be able to cross.
+
+Complete the carCrossing function to return true if and only if one car is crossing. */
+
+/* function carCrossing(aCrossing, bCrossing) {
+    if ((aCrossing && !bCrossing) || (bCrossing && !aCrossing)){
+        return true;
+    }else{
+        return false;
+    }
+} */
