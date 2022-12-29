9ae8048ea05b2922f57f9d26e59f4862c6da5c7e
rizankhan <rizankhan837@gmail.com>
2022-12-29 23:43:30 +0500

    Updated

--- alchemyTasks.js
+++ alchemyTasks.js
@@ -206,17 +206,17 @@ If the string is not a number, return 0. */
 
 /* Given two values a and b, combine them together as strings and return the result. The values may be a number, a boolean or a string. */
 
-function combineToString(a, b) {
+/* function combineToString(a, b) {
     let c = `${a}` + `${b}`;
     return c;
-}
+} */
 
 // Or
 
-/* function combineToString(a, b) {
+function combineToString(a, b) {
     let c = a.toString() + b.toString();
     return c;
-} */
+}
 
 /* Given some value a return whether or not the value is truthy */
 
