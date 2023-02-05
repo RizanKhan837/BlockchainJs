47b1a9583c3125f3bc98c0de5000e4d03585bef6
rizankhan <rizankhan837@gmail.com>
2023-02-05 16:48:37 +0500

    Update

--- alchemyDSA.js
+++ alchemyDSA.js
@@ -357,3 +357,33 @@ function largest(numbers) {
         return (accumulator >= currentValue) ? accumulator : currentValue;
     }, 1);
 } */
+
+/* Use the example provided in the details tab to guide you. You'll want to provide an initial value here that will eventually lead you towards your goal. Then you'll continue to accumulate non-duplicate numbers until you have an array that is full of only unique numbers. */
+
+/* function removeDuplicates(array) {
+    return array.reduce((accumulator, currentValue) => {
+        if(accumulator.indexOf(currentValue) === -1){
+            accumulator.push(currentValue);
+        }
+        return accumulator;
+    }, []);
+}
+
+console.log(removeDuplicates([1,2,3,4,5,1,2,3,4,5])); */
+
+// food is an array full of food objects
+// let's group them by "type" and return them
+
+/* Finish the function group. Sort each food by it's type and return an object in the format shown above.
+
+Don't depend on the types to only be "fruit" and "vegetable". We will be testing for additional food types in the test cases, so no hardcoding string values! */
+
+/* function group(food) {
+    return food.reduce((accumulator, currentValue) => {
+        if(accumulator[currentValue.type] === undefined){
+            accumulator[currentValue.type] = [];
+        }
+        accumulator[currentValue.type].push(currentValue);
+        return accumulator;
+    }, {});
+} */
