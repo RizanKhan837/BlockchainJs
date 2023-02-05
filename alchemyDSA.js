5183974d5dc39d334889579ab741a1c9ecf85b4c
rizankhan <rizankhan837@gmail.com>
2023-02-05 10:57:02 +0500

    Added Reduce Tasks

--- alchemyDSA.js
+++ alchemyDSA.js
@@ -334,3 +334,26 @@ function sum(numbers) {
     });
 }
 */
+
+/* It's time to complete the largest value function! Determine which is larger, the accumulator or the currentValue. Whichever is larger, return it! Then it will become the accumulator for the next iteration. */
+
+
+/* // numbers is an array full of numbers
+// let's find the largest and return it
+// i.e. [2,3,5,1,4] => 5
+function largest(numbers) {
+    return numbers.reduce((accumulator, currentValue) => {
+        return (accumulator >= currentValue) ? accumulator : currentValue;
+    });
+} */
+
+/* Just like the illustration above, we want our initial value to be 1. This way, if there are no positive values we will just return 1. */
+
+/* // numbers is an array full of numbers
+// let's find the largest and return it
+// i.e. [2,3,5,1,4] => 5
+function largest(numbers) {
+    return numbers.reduce((accumulator, currentValue) => {
+        return (accumulator >= currentValue) ? accumulator : currentValue;
+    }, 1);
+} */
